473615e3f0d5c832dc9855e6cb5f7d25622aaa50
Chelsea Zheng <51794314+czheng181920@users.noreply.github.com>
2024-09-22 17:21:05 -0400

    init

---
+++ logic.py
@@ -0,0 +1,14 @@
+# All of these functions are placeholders for the actual implementation.
+# Must also edit cli.py to import the user input for the required functions
+
+def db_create_meeting(meeting_id, title, date_time, location, details):
+    print("(to be implemented later)")
+
+def db_create_associated_calendar(meeting_id, calendar_id):
+    print("(to be implemented later)")
+
+def db_create_participant(participant_id, meeting_id, name, email):
+    print("(to be implemented later)")
+
+def db_create_attachment(attachment_id, meeting_id, attachment_url):
+    print("(to be implemented later)")
